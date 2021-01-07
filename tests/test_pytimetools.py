5c6df26607b79f2be7bf1e387ff0e8f0780990f9
huoyinghui <yhhuo@yunjinginc.com>
2021-01-07 09:44:13 +0800

    add: requirements

--- tests/test_pytimetools.py
+++ tests/test_pytimetools.py
@@ -3,7 +3,7 @@ from pytimetools import utctools
 
 
 def test_version():
-    assert __version__ == '0.1.3.3'
+    assert __version__ == '0.1.3.4'
 
 # 测试时间转换
 
@@ -11,4 +11,4 @@ def test_version():
 def test_utc_to_gmt():
     utc_now = utctools.get_now()
     gmt_now = utctools.utc_to_gmt(utc_now)
-    assert gmt_now == ""
+    assert gmt_now != ""
